4a31c36bbd23121ee7c48b4926ac20200827714e
Benjamin Leguizamon <benjaleguizamon92@gmail.com>
2026-09-21 14:48:11 -0300

    Add HUD and start/game over panel handling

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text speedText;
+    [SerializeField] private GameObject startPanel;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button retryButton;
+
+    private string scoreTemplate;
+    private string speedTemplate;
+
+    private void Awake()
+    {
+        scoreTemplate = scoreText.text;
+        speedTemplate = speedText.text;
+    }
+
+    private void OnEnable()
+    {
+        gameManager.Changed += Refresh;
+        playButton.onClick.AddListener(gameManager.StartGame);
+        retryButton.onClick.AddListener(gameManager.StartGame);
+        Refresh();
+    }
+
+    private void OnDisable()
+    {
+        gameManager.Changed -= Refresh;
+        playButton.onClick.RemoveListener(gameManager.StartGame);
+        retryButton.onClick.RemoveListener(gameManager.StartGame);
+    }
+
+    private void Refresh()
+    {
+        scoreText.text = scoreTemplate.Replace("{value}", gameManager.Score.ToString());
+        speedText.text = speedTemplate.Replace("{value}", gameManager.CurrentSpeed.ToString("0.0"));
+        startPanel.SetActive(gameManager.State == GameManager.GameState.Ready);
+        gameOverPanel.SetActive(gameManager.State == GameManager.GameState.GameOver);
+    }
+}
